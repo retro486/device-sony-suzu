078090e9261313e7049d748540d00fac22efa454
Russ Bernhardt <russell.bernhardt@gmail.com>
2019-06-15 09:47:08 -0700

    Add F-Droid privileged extension

--- device.mk
+++ device.mk
@@ -88,6 +88,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     TransPowerSensors
 
+# F-Droid
+PRODUCT_PACKAGES += \
+    FDroidPrivilegedExtension
+
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
